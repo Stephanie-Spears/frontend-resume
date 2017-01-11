871281c6c46650cf3c65b73b3eb2a0116fb15154
Stephanie Spears <stephanie.spears1@gmail.com>
2017-01-11 04:21:23 -0800

    remove syntax error in javascript

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,15 +4,18 @@
 var name = "Stephanie Spears";
 var role = "Web Developer";
 var skills = ["PHP", ".NET", "Drupal", "HTML/CSS/JavaScript", "C++", "Excellent written and verbal communication", "Highly organized and detail-oriented", "Learn and adapt quickly", "Good sense of humor and positive attitude", "Strong work ethic", "Analytical thinking and research ability", "Work effectively as part of a team or independently"];
+var me = "img/me.jpg";
 var email = "Stephanie.Spears11@gmail.com";
-var phone = "503-459-xxxx"
-var me = "img/me.jpg"
-var contact = email + phone;
-var welcome = "Hi there!"
-var bio = {
-  "name" : name, "role" : role, "contact" : contact, "me": me, "skills" : skills, "welcome" : welcome,
-};
+var phone = "503-459-xxxx";
+// var location = "Portland, Oregon";
 
+var contact = email + " " + phone;
+
+var welcome = "Hi there!";
+var bio = {
+  "name" : name, "role" : role, "contact" : contact, "me": me, "skills" : skills, "welcome" : welcome, "location" : location
+};
+//combine elements in an array into a single string using array.join([chars]), separated by an optional chars
 
 var formattedName=HTMLheaderName.replace("%data%", name);
 
@@ -23,7 +26,7 @@ $("#header").prepend(formattedName, formattedRole);
 // $("#main").append(skills);
 
 
-$("#main").append(bio.contact);
+// $("#main").append();
 
 
 
