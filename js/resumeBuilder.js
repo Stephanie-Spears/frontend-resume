7925bc0b34fac541eced99dfa307905c4520340d
Stephanie Spears <stephanie.spears1@gmail.com>
2017-01-11 12:07:03 -0800

    add resume content

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,66 +2,71 @@ var bio = {
   "name": "Stephanie Spears",
   "role": "Web Developer",
   "contacts": {
-    "mobile": "503-xxx-4077",
+    "mobile": "555-555-5555",
     "email": "Stephanie.Spears22@gmail.com",
     "github": "https://github.com/Stephanie-Spears",
     "location": "Portland, Oregon"
   },
-  "welcomeMsg": "lorem ipsum dolor sit amet etc etc etc.",
+  "welcomeMsg": "three ways to declare objects: var myObj={ }, myObj.property=some value, and myObj['property']=some value",
   "skills": [
     "PHP", ".NET", "Drupal", "HTML/CSS/JavaScript", "C++", "Excellent written and verbal communication", "Highly organized and detail-oriented", "Learn and adapt quickly", "Good sense of humuor and positive attitude", "Strong work ethic", "Analytical thinking and research ability", "Work effectively as part of a team or independently"
   ],
-  "bioPic": "img/bioPic.jpg"
+  "myBioPic": "img/bioPic.jpg"
 };
+
+
+var work = {
+  "workStart": "XXXXX",
+  "workEmployer": "Kuzuha Language College",
+  "workTitle": "English Language Consultant",
+  "workDates": "2012-2015",
+  "workLocation": "Osaka",
+  "workDescription": "English Language Consultant on a variety of technical and academnic materials. Editing, proof-reading, translation."
+};
+
+var school = {
+  "schoolStart": "XXXX",
+  "schoolName": "Epicodus",
+  "schoolDegree": "graduate",
+  "schoolLocation": "Portland, Oregon",
+  "schoolMajor": "PHP/Drupal/JavaScript"
+};
+
+// var school2 = new school();
 
 var formattedName=HTMLheaderName.replace("%data%", bio.name);
 var formattedRole=HTMLheaderRole.replace("%data%", bio.role);
+$("#header").prepend(formattedName, formattedRole);
 
 var formattedMobile=HTMLmobile.replace("%data%", bio.contacts.mobile);
 var formattedEmail=HTMLemail.replace("%data%", bio.contacts.email);
 var formattedGithub=HTMLgithub.replace("%data%", bio.contacts.github);
 var formattedLocation=HTMLlocation.replace("%data%", bio.contacts.location);
-
-var formattedBioPic=HTMLbioPic.replace("%data%", bio.bioPic);
-var formattedWelcomeMsg=HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
-
-
-
-// var formattedPicture=HTMLbioPic.replace("%data%", bioPic);
-// var formattedSkills=HTMLskills.replace("%data%", skills);
-// var formattedWelcome=HTMLwelcomeMsg.replace("%data%", welcomeMsg);
-
-$("#header").prepend(formattedName, formattedRole);
 $("#topContacts").prepend(formattedMobile, formattedEmail, formattedGithub, formattedLocation);
-$("#").append(formattedBioPic, formattedWelcomeMsg);
 
-// $("#header").append(formattedContact);
-
-
-// $("#main").append("Stephanie Spears");
-
-// $("#main").prepend(formattedName, formattedRole);
+var formattedBioPic=HTMLbioPic.replace("%data%", bio.myBioPic);
+var formattedWelcomeMsg=HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
+$("#header").append(formattedWelcomeMsg, formattedBioPic);
 
 
+var formattedWork=HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
+$("#header").append(formattedWelcomeMsg, formattedBioPic);
 
-// var firstName="Stephanie";
-// var age=27;
-// console.log(firstName);
-//
-// var awesomeThoughts="I am Stephanie and I am awesome!";
-//
-// console.log(awesomeThoughts);
+
+
+
+// var formattedSkills=HTMLskills.replace("%data%", bio.skills);
+// $("#topContacts").append(formattedSkills, HTMLskillsStart);
+
+
+
+
+
 
 //[string].replace([old], [new]);
 //var email = "cameron@udacity.com";
 //var newEmail = email.replace("udacity", "gmail");
 //console.log(email);
 //console.log(newEmail);
-
-// var funThoughts= awesomeThoughts.replace("awesome", "fun");
-// console.log(funThoughts);
-//
-// $("#main").append(funThoughts);
-
 
 //combine elements in an array into a single string using array.join([chars]), separated by an optional chars
